103a68527ace94099a27081a18eb09cedeac9279
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2023-08-21 08:28:05 +0530

    Initial Configuration

---
+++ src/autoload.php
@@ -0,0 +1,4 @@
+<?php
+require __DIR__ . '/models/Room.php';
+require __DIR__ . '/models/Hosteller.php';
+require __DIR__ . '/models/Employee.php';
---
+++ src/env-copy.php
@@ -0,0 +1,5 @@
+<?php
+define('SERVER', 'localhost');
+define('USERNAME', '');
+define('PASSWORD', '');
+define('DATABASE', 'hms_2305');
