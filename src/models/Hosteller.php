b4e9127a01497c728442aba5e3a221e101fc3771
Shubham Kumar <shubhamkumar.sk9502@gmail.com>
2024-01-14 15:46:03 +0530

    change

--- src/models/Hosteller.php
+++ src/models/Hosteller.php
@@ -27,24 +27,16 @@ class Hosteller
         $gender = $Hosteller['gender'];
         $date_of_birth = $Hosteller['date_of_birth'];
         $contact_no = $Hosteller['contact_no'];
-<<<<<<< HEAD
         $email = $Hosteller['email'];
         $fathers_name = $Hosteller['fathers_name'];
         $sql = "INSERT INTO Hosteller(name, gender, date_of_birth, contact_no, email,fathers_name) VALUES('$name', '$gender', '$date_of_birth', '$contact_no', '$email','$fathers_name')";
-=======
-        $sql = "INSERT INTO hosteller(first_name, middle_name, last_name, contact_no) VALUES('$first_name', '$middle_name', '$last_name', '$contact_no')";
->>>>>>> 247cc2ec84ec1a992714db982b45b648c11016c6
         $this->connection->query($sql);
         header("location:hosteller.php");
     }
 
     public function details(int $id)
     {
-<<<<<<< HEAD
         $sql = "SELECT * FROM Hosteller WHERE  hosteller_id=$id";
-=======
-        $sql = "SELECT * FROM hosteller WHERE id=$id";
->>>>>>> 247cc2ec84ec1a992714db982b45b648c11016c6
         $result = $this->connection->query($sql);
         if ($result->num_rows > 0) {
             return $result->fetch_assoc();          
@@ -59,25 +51,17 @@ class Hosteller
         $gender = $Hosteller['gender'];
         $date_of_birth = $Hosteller['date_of_birth'];
         $contact_no = $Hosteller['contact_no'];
-<<<<<<< HEAD
         $email = $Hosteller['email'];
         $fathers_name=$Hosteller['fathers_name'];
 
         $sql = "UPDATE Hosteller SET name='$name', gender='$gender', date_of_birth='$date_of_birth', contact_no='$contact_no', email='$email',fathers_name='$fathers_name' WHERE Hosteller_id=$Hosteller_id";
-=======
-        $sql = "UPDATE hosteller SET first_name='$first_name', middle_name='$middle_name', last_name='$last_name', contact_no='$contact_no' WHERE id=$id";
->>>>>>> 247cc2ec84ec1a992714db982b45b648c11016c6
         $this->connection->query($sql);
         header("location:hosteller.php");
     }
 
     public function delete(int $Hosteller_id)
     {
-<<<<<<< HEAD
         $sql = "DELETE FROM Hosteller WHERE hosteller_id=$Hosteller_id";
-=======
-        $sql = "DELETE FROM hosteller WHERE id=$id";
->>>>>>> 247cc2ec84ec1a992714db982b45b648c11016c6
         $this->connection->query($sql);
         header("location:hosteller.php");
     }
